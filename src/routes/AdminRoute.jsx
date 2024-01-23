ae6732ab6485fe97163628348f646112e7688108
Arjun Raj <71980597+zuko046@users.noreply.github.com>
2024-01-23 14:23:21 +0530

    hai

--- src/routes/AdminRoute.jsx
+++ src/routes/AdminRoute.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-
 import HomeLayoutPage from "../pages/HomeLayoutPage";
 import Dashboard from "../pages/element/Dashboard";
 import Payment from "../pages/element/Payment";
@@ -9,7 +8,7 @@ const AdminRoute = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomeLayoutPage />}>
+        <Route path="*" element={<HomeLayoutPage />}>
           <Route index element={<Dashboard />} />
           <Route path="payment" element={<Payment />} />
           {/* rest of the routes ... */}
