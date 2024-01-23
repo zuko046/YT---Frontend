adad72eddec1530887ede21facbb191a23b89c01
Arjun Raj <71980597+zuko046@users.noreply.github.com>
2024-01-23 14:00:06 +0530

    NOTHING SPL

--- src/pages/HomeLayoutPage.jsx
+++ src/pages/HomeLayoutPage.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import Header from "../components/header/Header";
+import Sidebar from "../components/sidebar/Sidebar";
+import { Outlet } from "react-router-dom";
 
 const HomeLayoutPage = () => {
   return (
     <>
-    
+      <Header />
+      <Sidebar />
+      <Outlet />
     </>
-  )
-}
+  );
+};
 
-export default HomeLayoutPage
+export default HomeLayoutPage;
