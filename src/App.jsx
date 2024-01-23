57b41943569b7d97fd7049c701b3adc77be8b569
Arjun Raj <71980597+zuko046@users.noreply.github.com>
2024-01-23 17:28:39 +0530

    ai

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/" element={<AdminRoute />} />
+          <Route path="*" element={<AdminRoute />} />
         </Routes>
       </Router>
     </>
